1c07171f30d4e8faa0b8d63c7adb0619391c5dbf
robertchou91 <robertchou91@gmail.com>
2018-01-29 15:39:22 -0800

    first commit of the d day

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import News from './News/News';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <News />
+      </div>
     );
   }
 }
---
+++ src/News/News.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const news = (props) => {
+  return (
+    <div>
+      <header>
+        This is a header.
+      </header>
+      <body>
+        <div>
+          <p>Recent News</p>
+        </div>
+      </body>
+    </div>
+  );
+};
+
+export default news;

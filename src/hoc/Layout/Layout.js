42132c1eeef87365b6b7b4717740e11c77e01074
robertchou91 <robertchou91@gmail.com>
2018-01-30 17:30:13 -0800

    3rd commit of the day

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../Aux/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>

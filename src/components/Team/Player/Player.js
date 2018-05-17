7b564f1f2bdbeff1370ec6918c6e0ebe2991f975
robertchou91 <robertchou91@gmail.com>
2018-05-16 23:18:11 -0700

    3rd commit

--- src/components/Team/Player/Player.js
+++ src/components/Team/Player/Player.js
@@ -10,6 +10,8 @@ const player = (props) => {
       <div className={classes.Info_playerinformation}>
         <h1>{props.playerName}</h1>
         <h3>{props.realName}</h3>
+        <a href={props.playerTwitter} className={classes.Twitter}><img src="imgs/Twitter.png" alt="Twitter" /></a>
+        <a href={props.playerTwitch} className={classes.Twitch}><img src="imgs/Twitch.png" alt="Twitch" /></a>
       </div>
     </div>
   );

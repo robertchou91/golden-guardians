87a6dfcb9f4236b998ad50b0484ea638bb8a2b42
robertchou91 <robertchou91@gmail.com>
2018-05-16 23:12:55 -0700

    2nd commit

--- src/components/Team/PlayerDesktop/PlayerDesktop.js
+++ src/components/Team/PlayerDesktop/PlayerDesktop.js
@@ -11,7 +11,8 @@ const player = (props) => {
         <h1>{props.playerrealName}</h1>
         <h2>{props.playerBio}</h2>
         <div className={classes.Info_social}>
-          <a href={props.playerTwitter}><img src="imgs/Twitter.png" alt="Twitter" /></a>
+          <a href={props.playerTwitter} className={classes.Twitter}><img src="imgs/Twitter.png" alt="Twitter" /></a>
+          <a href={props.playerTwitch} className={classes.Twitch}><img src="imgs/Twitch.png" alt="Twitch" /></a>
         </div>
       </div>
     </div>

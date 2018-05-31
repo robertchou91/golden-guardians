9b1c0e893422c38c213280a1d7269cece20718c5
robertchou91 <robertchou91@gmail.com>
2018-05-30 22:11:35 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename="/golden-guardians">
     <App />
   </BrowserRouter>
 );
